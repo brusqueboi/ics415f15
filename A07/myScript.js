2accbec25e639cad28674a88169b91445a0d47b4
brusqueboi <jdalton@hawaii.edu>
2015-09-22 08:55:37 -1000

    A07

---
+++ A07/myScript.js
@@ -0,0 +1,16 @@
+/**
+ * Created by brusqueboi on 9/22/15.
+ */
+function getClasses(elem){
+  var myArray = new Array(document.getElementsByClassName("elem"));
+  document.getElementById("elem").innerHTML = myArrayy;
+}
+
+function addClass(elem, className){
+  myArray.push(elem);
+
+}
+
+function validateForm(){
+  
+}
